3fde14ef0286ebc87c1b5f3475d4204bbb4fc289
X <brandonxpeterson@gmail.com>
2023-09-11 14:41:57 -0400

    git add .

--- Week 3/chpt4_pc5.cpp
+++ Week 3/chpt4_pc5.cpp
@@ -3,23 +3,23 @@ using namespace std;
 
 int main() 
 {
-
+    //defining varibles
     string consent;
     double bmi, weight, height;
-
+    //making sure user wants to share personal information
     cout << "Hello beautiful. Would you like to share you hieght and weight? y/n \n";
     cin >> consent; 
 
     if (consent == "y")
     {
-
+        //inserting user's information
         cout << "What is your height? (inches) \n";
         cin >> height;
         cout << "What is your weight? (pounds) \n";
         cin >> weight;
-
+        //calculating BMI
         bmi = weight * 703 / (height * height) ;
-
+        //results and display
         if ( bmi >= 18.5 && bmi <= 25 )
         {
             cout << "You are an optimal weight. \n";
@@ -43,7 +43,7 @@ int main()
     }
 
     else {
-
+        //message for those who decided not to calculate BMI (s/o Dway)
         cout << "Have a great day, beloved!";
 
     }
--- Week 3/chpt4_pc9.cpp
+++ Week 3/chpt4_pc9.cpp
@@ -3,17 +3,19 @@ using namespace std;
 
 int main() 
 {
- 
+    //defining varibles
     string consent;
     int p, n, d, q, nickel, dime, quarter, total;
 
+    //making sure my user wants to start playing the game
     cout << "Do you want to play my Change for a Dollar game? y/n \n";
     cin >> consent; 
 
     if (consent == "y")
     {
-
+        //game instructions
         cout << "You have to enter the amount of coins required to make EXACTLY one dollar. \n"
+        //let the games begin
         << "How many pennies? \n";
         cin >> p;
 
@@ -26,46 +28,38 @@ int main()
         cout<< "How many quartes? \n";
         cin >> q;
 
+        //creating new varibles so that inserted values can be processed correctly
         nickel = 5 * n;
         dime = 10 * d;
         quarter = 25 * q;
         total = p + nickel + dime + quarter;
 
-
+        //game results
         if ( total == 100)
         {
-
             cout << "YOU WON THE GAME! \n";
-
         }
 
         else if ( total < 100)
         {
-            
             cout << "The amount you enter is not enough. \n";
-
         }
         
         else if ( total > 100)
         {
-
              cout << "The amount you enter is too much. \n";
-
         }
 
         else 
         {
-
              cout << "INVALID AMOUNT. \n";
-
         }
 
     }
 
     else {
-
+        //message for those who decided not to play the game (s/o Dway)
         cout << "Have a great day, beloved!";
-
     }
 
     return 0;
