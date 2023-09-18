35eba06c412835e9ec2e72d8bcb7a415ae3c652e
xpete3 <brandonxpeterson@gmail.com>
2023-09-18 09:00:21 -0400

    Week 4 assignment

---
+++ Week 4/chpt5_pc1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+int user_number,
+        total = 0;
+
+    cout << "\nEnter a positive number: ";
+    cin >> user_number;
+
+    while (user_number < 0)
+    {
+        cout << "ERROR: a positive number must be chosen\n";
+        cout << "Enter a positive number: ";
+        cin >> user_number; 
+    }
+
+    for (int i = 1; i <= user_number; i++)
+    {
+        total += i;
+    }
+
+    cout << "Total = " 
+         << total ;
+
+    return 0;
+    
+}
